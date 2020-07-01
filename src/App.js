42bd5a26ca28cc753842b0313baad75a31e06df9
Sayaka Ono <sayaka.ono.81@gmail.com>
2020-07-01 16:06:05 -0700

    complete app with tests

--- src/App.js
+++ src/App.js
@@ -2,8 +2,45 @@ import React from 'react';
 import './App.css';
 
 class App extends React.Component {
+  state = {
+    counter: 0,
+    error: false
+  };
+
+  onIncrementClick = () => {
+    if (this.state.error) {
+      this.setState({ error: false });
+    }
+    this.setState({ counter: this.state.counter + 1 });
+  };
+
+  onDecrementClick = () => {
+    if (this.state.counter === 0) {
+      this.setState({ error: true });
+    } else {
+      this.setState({ counter: this.state.counter - 1 });
+    }
+  };
+
   render() {
-    return <div data-test="component-app"></div>;
+    return (
+      <div data-test="component-app">
+        <h1 data-test="counter-display">
+          The counter is currently {this.state.counter}
+        </h1>
+        {this.state.error && (
+          <p data-test="error-message" style={{ color: 'red' }}>
+            The counter cannot go below 0
+          </p>
+        )}
+        <button data-test="increment-button" onClick={this.onIncrementClick}>
+          increment counter
+        </button>
+        <button data-test="decrement-button" onClick={this.onDecrementClick}>
+          decrement counter
+        </button>
+      </div>
+    );
   }
 }
 
